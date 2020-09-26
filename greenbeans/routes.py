5dae7d8906e7efe1a8a7e4234d2cab3ef1a301f1
Lorenz <lorenz.kort@gmail.com>
2020-09-26 13:19:20 +0200

    Added post update and delete functions

--- greenbeans/routes.py
+++ greenbeans/routes.py
@@ -1,30 +1,16 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from greenbeans import app, db, bcrypt
-from greenbeans.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from greenbeans.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from greenbeans.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author':'Corey Shafer',
-        'title':'blog post 1',
-        'content': 'first post content',
-        'date':'April 20 2020'
-    },
-    {
-        'author':'John Doe',
-        'title':'blog post 2',
-        'content': 'second post content',
-        'date':'April 25 2020'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -72,11 +58,13 @@ def save_picture(form_picture):
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
+    # Resize picture
     output_size = (125,125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
 
+    # Remove previous profile picture
     prev_picture = os.path.join(app.root_path, 'static/profile_pics', current_user.image_file)
     if os.path.exists(prev_picture):
         os.remove(prev_picture)
@@ -101,3 +89,49 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static' , filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=["GET","POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=["GET","POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':    
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
