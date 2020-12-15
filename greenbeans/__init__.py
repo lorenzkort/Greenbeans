abd8d20e5275b88b81dc6a7d422600b5f8176141
Lorenz <lorenz.kort@gmail.com>
2020-12-15 22:24:52 +0100

    Edited greenbeans

--- greenbeans/__init__.py
+++ greenbeans/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '04059ffd6ab2a3ab32f5d2a09b950242'
@@ -11,5 +13,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 578
+app.config['USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER') 
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from greenbeans import routes
